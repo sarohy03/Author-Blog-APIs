c8fafe1873f7c6b88512d83468569117195c86a3
sarohy <a.h.sarohy@gmail.com>
2024-08-11 18:29:15 +0500

    change

--- Config.py
+++ Config.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
-CONNECTION_STRING = ("mongodb+srv://sarohy:mamdoot222@cluster0.i2pig5w.mongodb.net/?retryWrites=true&w=majority&appName"
-                     "=Cluster0&tlsAllowInvalidCertificates=true")
+CONNECTION_STRING = ("mongodb+srv://sarohy:mamdoot222@cluster0.i2pig5w.mongodb.net/?retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true")
+
 
 client = MongoClient(CONNECTION_STRING)
 
